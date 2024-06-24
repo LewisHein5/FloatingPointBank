9d5d1ea9f891a937d630029cedf1b469b1c08355
Lewis Hein <lewishein5@gmail.com>
2024-06-24 18:22:43 -0500

    Delete most of the app to see what happens

--- FloatingPointBank/Program.cs
+++ FloatingPointBank/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FloatingPointBank;
-using FloatingPointBank.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<AccountUpdateService<short>>();
-builder.Services.AddScoped<AccountUpdateService<float>>();
+
 
 await builder.Build().RunAsync();
